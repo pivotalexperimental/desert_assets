76f3216c3007b68023dd3e0d44221a424ab323b4
Corey Innis <corey@mowgli.local>
2008-09-09 02:14:15 -0700

    Hacked in a change to ImagesController#render to allow it to render actual binary images generated from the view template (can also handle static images, but they need a builder file extension for now).  Added an example view, rails_version.png.erb, to illustrate.

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,3 +1,26 @@
 class ImagesController < AssetsController
+  def render_with_image_view(options = nil, extra_options = {}, &block)
+    params = request.parameters
+    format = params[:format]
+    options = {
+      :status => 200,
+      :filename => "#{params[:action]}.#{format}",
+      :disposition => 'inline',
+      :type => "image/#{format}"
+    }
+    
+    data = render_to_string
+    send_file_headers! options.merge(:text => data, :length => data.size)
+    @performed_render = false
+    render_without_image_view options
+  end
+  alias_method_chain :render, :image_view
   
+  def render_to_string(options = nil, &block)
+    render_without_image_view(options, &block)
+  ensure
+    erase_render_results
+    forget_variables_added_to_assigns
+    reset_variables_added_to_assigns
+  end
 end
